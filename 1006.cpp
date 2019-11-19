8c8bbcb668081e881fd5e5676bca0ffe148b4471
τ.呆了个呆 <yinsuhu@users.noreply.github.com>
2019-11-19 11:37:14 +0800

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+/**
+ * desc: 1006.捡石头
+ * author: yinsuhu
+ * date: 2019.11.19
+ */
+#include<iostream>
+using namespace std;
+int main() {
+    int r1, r2, r3;
+    cin >> r1 >> r2 >> r3;
+    cout << 20 - r1 - r2 - r3 << endl;
+    return 0;
+}
